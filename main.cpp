52e68d6166c76f21ff673e8c0014e3649ea45956
Kartik Mahure <131333372+Kartikmahure11@users.noreply.github.com>
2023-05-12 20:09:02 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << " NAME     : RAM SHARMA" << endl;
-    cout << " ADDRESS  : NAGPUR    " << endl;
+    float perimeter,area;
+    int r=6;
+    perimeter=2*3.14*6;
+    cout << "perimeter of the circle :"<< perimeter << endl;
+    area=3.14*r*r;
+    cout << "area of the circle : "<<area<< endl;
     return 0;
 }
